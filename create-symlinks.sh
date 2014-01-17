f2ca96c5a9365d3e969f9465e4157699df372529
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-01-17 10:54:03 +0100

    random stuff

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-ZSH_FOLDER="$HOME/Dropbox/synced-config/oh-my-zsh"
+ZSH_FOLDER="$HOME/Dropbox/synced-configs/oh-my-zsh"
 ZSH_FILES=( aliases zprofile zshrc )
 
 for file in $ZSH_FILES
@@ -12,7 +12,7 @@ ln -s $ZSH_FOLDER/theme $HOME/.oh-my-zsh/themes/leimi.zsh-theme
 
 
 
-CONFIG_ROOT="$HOME/Dropbox/synced-config"
+CONFIG_ROOT="$HOME/Dropbox/synced-configs"
 CONFIG_FOLDERS=( sublime-text-2 sublime-text-3 openbox lxsession pytyle cairo-dock )
 for folder in $CONFIG_FOLDERS
 do

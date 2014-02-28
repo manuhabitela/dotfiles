1bc3775550f3c14e947e9e3c3f044e713b359434
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-02-28 12:30:14 +0100

    st3: adding the current filename in status bar (for fullscreen coding)

---
+++ sublime-text-3/Packages/User/current_page_status.py
@@ -0,0 +1,6 @@
+import sublime
+import sublime_plugin
+
+class CurrentPathStatusEvent(sublime_plugin.EventListener):
+    def on_activated(self, view):
+        view.set_status('currentPath', view.file_name())

949f198b2cf8246a778910e34ce42d8fc71ce48a
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2014-08-05 12:09:56 +0200

    sublime text 3 misc changes

--- .config/sublime-text-3/Packages/User/current_page_status.py
+++
@@ -1,6 +0,0 @@
-import sublime
-import sublime_plugin
-
-class CurrentPathStatusEvent(sublime_plugin.EventListener):
-    def on_activated(self, view):
-        view.set_status('currentPath', view.file_name())

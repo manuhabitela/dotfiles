ac21669b64281eda6927a6ea4262b82c1882ce20
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2016-05-23 14:35:04 +0200

    yolo commit: changes in misc programs

--- .zshrc
+++ .zshrc
@@ -1,4 +1,5 @@
-PATH="/bin"
+PATH="$PATH:$HOME/.rvm/bin"
+PATH="$PATH/bin"
 PATH="$PATH:/sbin"
 PATH="$PATH:/usr/bin"
 PATH="$PATH:/usr/game"
@@ -6,19 +7,21 @@ PATH="$PATH:/usr/sbin"
 PATH="$PATH:/usr/local/bin"
 PATH="$PATH:/usr/local/sbin"
 PATH="$PATH:/usr/lib/lightdm/lightdm"
-PATH="$PATH:$HOME/.rvm/bin"
+PATH="$PATH:/usr/bin/core_perl/"
+PATH="$PATH:/usr/local/go/bin"
+PATH="$PATH:$HOME/.composer/vendor/bin"
+if [[ -a $HOME/Dev/ecolutis/dkr/bin ]]; then
+    PATH="$PATH:$HOME/Dev/ecolutis/dkr/bin"
+fi
+if [[ -a $HOME/Dev/ecolutis/www/cmd/bin ]]; then
+    PATH="$PATH:$HOME/Dev/ecolutis/www/cmd/bin"
+fi
 export PATH="$PATH:$HOME/bin"
-
-source_sh () {
-  emulate -LR bash
-  . "$@"
-}
 
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
 # Themes: ~/.oh-my-zsh/themes/
-# ZSH_THEME="robbyrussell"
 ZSH_THEME="leimi"
 
 COMPLETION_WAITING_DOTS="true"
@@ -26,13 +29,17 @@ COMPLETION_WAITING_DOTS="true"
 # Plugins: ~/.oh-my-zsh/plugins/
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git nyan)
+plugins=(git nyan ssh-agent colored-man-pages)
+
+zstyle :omz:plugins:ssh-agent agent-forwarding on
 
 source $ZSH/oh-my-zsh.sh
 
 setopt auto_cd
-cdpath=($HOME/Dev $HOME/Dev/httpd $HOME/Dev/vagrant $HOME/Dropbox/Web/localhost)
+cdpath=($HOME/Dev $HOME/Dev/ecolutis $HOME/Dev/ecolutis/www/ecolutis.dev $HOME/Dropbox/Web/localhost)
 
-source $HOME/.aliases
+source $HOME/.zsh_aliases
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+if [[ -a $HOME/Dev/ecolutis/dkr ]]; then
+    export EDKR_HOME="$HOME/Dev/ecolutis/dkr"
+fi
--- create-symlinks.sh
+++ create-symlinks.sh
@@ -2,7 +2,7 @@
 
 DOTFILES="/home/manu/Dropbox/dotfiles"
 
-FILES=( bin .aliases .gtkrc-2.0 .xmodmaprc .zshrc )
+FILES=( bin .aliases .gtkrc-2.0 .xmodmaprc .zshrc .ackrc .fonts .dmenurc .Xresources )
 
 for FILE in $FILES
 do
@@ -10,18 +10,29 @@ do
 	ln -s $DOTFILES/$FILE $HOME/$FILE
 done
 
-CONFIGS=( awesome cairo-dock dunst gtk-2.0 gtk-3.0 lxsession nitrogen openbox pytyle roxterm.sourceforge.net sublime-text-2 sublime-text-3 volumeicon xfce4 )
+CONFIGS=( awesome dunst gtk-2.0 gtk-3.0 git-cola lxsession nitrogen roxterm.sourceforge.net volumeicon xfce4 )
 for CONFIG in $CONFIGS
 do
 	echo "ln -s $DOTFILES/.config/$CONFIG $HOME/.config/$CONFIG"
 	ln -s $DOTFILES/.config/$CONFIG $HOME/.config/$CONFIG
 done
 
-echo "ln -s $DOTFILES/.themes/ACIDBoxCustom $HOME/.themes/ACIDBoxCustom"
-ln -s $DOTFILES/.themes/ACIDBoxCustom $HOME/.themes/ACIDBoxCustom
-
 echo "ln -s $DOTFILES/.themes/Numix $HOME/.themes/Numix"
 ln -s $DOTFILES/.themes/Numix $HOME/.themes/Numix
 
 echo "ln -s $DOTFILES/.oh-my-zsh/themes/leimi.zsh $HOME/.oh-my-zsh/themes/leimi.zsh-theme"
 ln -s $DOTFILES/.oh-my-zsh/themes/leimi.zsh $HOME/.oh-my-zsh/themes/leimi.zsh-theme
+
+
+
+mkdir $HOME/.config/sublime-text-3
+mkdir $HOME/.config/sublime-text-3/Packages
+mkdir $HOME/.config/sublime-text-3/Local
+echo "ln -s $DOTFILES/.config/sublime-text-3/Packages/User $HOME/.config/sublime-text-3/Packages/User"
+ln -s $DOTFILES/.config/sublime-text-3/Packages/User $HOME/.config/sublime-text-3/Packages/User
+
+echo "ln -s $DOTFILES/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2 $HOME/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2"
+ln -s $DOTFILES/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2 $HOME/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2
+
+echo "ln -s $DOTFILES/.config/sublime-text-3/Local/License.sublime_license $HOME/.config/sublime-text-3/Local/License.sublime_license"
+ln -s $DOTFILES/.config/sublime-text-3/Local/License.sublime_license $HOME/.config/sublime-text-3/Local/License.sublime_license

5d720ae60b417f8af4757d88e5f21c52bd7a2ad1
Emmanuel Pelletier <e.pelletier@vtech.fr>
2016-09-22 15:24:07 +0200

    removing unused configs: cairodock, openbox, pytyle, etc

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-DOTFILES="/home/manu/Dropbox/dotfiles"
+DOTFILES="/home/manu/dotfiles"
 
-FILES=( bin .aliases .gtkrc-2.0 .xmodmaprc .zshrc .ackrc .fonts .dmenurc .Xresources )
+FILES=( bin .aliases .gtkrc-2.0 .xmodmaprc .zshrc .ackrc .dmenurc .Xresources )
 
 for FILE in $FILES
 do
@@ -10,15 +10,12 @@ do
 	ln -s $DOTFILES/$FILE $HOME/$FILE
 done
 
-CONFIGS=( awesome dunst gtk-2.0 gtk-3.0 git-cola lxsession nitrogen roxterm.sourceforge.net volumeicon xfce4 )
+CONFIGS=( awesome dunst gtk-3.0 lxsession )
 for CONFIG in $CONFIGS
 do
 	echo "ln -s $DOTFILES/.config/$CONFIG $HOME/.config/$CONFIG"
 	ln -s $DOTFILES/.config/$CONFIG $HOME/.config/$CONFIG
 done
-
-echo "ln -s $DOTFILES/.themes/Numix $HOME/.themes/Numix"
-ln -s $DOTFILES/.themes/Numix $HOME/.themes/Numix
 
 echo "ln -s $DOTFILES/.oh-my-zsh/themes/leimi.zsh $HOME/.oh-my-zsh/themes/leimi.zsh-theme"
 ln -s $DOTFILES/.oh-my-zsh/themes/leimi.zsh $HOME/.oh-my-zsh/themes/leimi.zsh-theme
@@ -30,9 +27,3 @@ mkdir $HOME/.config/sublime-text-3/Packages
 mkdir $HOME/.config/sublime-text-3/Local
 echo "ln -s $DOTFILES/.config/sublime-text-3/Packages/User $HOME/.config/sublime-text-3/Packages/User"
 ln -s $DOTFILES/.config/sublime-text-3/Packages/User $HOME/.config/sublime-text-3/Packages/User
-
-echo "ln -s $DOTFILES/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2 $HOME/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2"
-ln -s $DOTFILES/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2 $HOME/.config/sublime-text-3/Packages/Theme\ -\ Cobalt2
-
-echo "ln -s $DOTFILES/.config/sublime-text-3/Local/License.sublime_license $HOME/.config/sublime-text-3/Local/License.sublime_license"
-ln -s $DOTFILES/.config/sublime-text-3/Local/License.sublime_license $HOME/.config/sublime-text-3/Local/License.sublime_license

09debc0e19f11269a8d7ff3b75c3dda3dde026cd
Emmanuel Pelletier <e.pelletier@vtech.fr>
2020-03-27 18:38:51 +0100

    awesome menu: custom lxde logout modal box

--- .config/awesome/includes/main_menu.lua
+++ .config/awesome/includes/main_menu.lua
@@ -1,7 +1,7 @@
 local xdg_menu = require("archmenu")
 
 table.insert(xdgmenu, { "Terminal", terminal })
-table.insert(xdgmenu, { "Quitter", "lxde-logout" })
+table.insert(xdgmenu, { "Quitter", "/home/manu/bin/lxde-logout" })
 main_menu = awful.menu.new({ items = xdgmenu, theme = { height = 22, width = 200 } })
 
 client.connect_signal("button::press", function(c)

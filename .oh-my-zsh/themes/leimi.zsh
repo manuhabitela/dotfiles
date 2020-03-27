cfe656b1f7dd12f14c9171165de72b208a34779b
Emmanuel Pelletier <e.pelletier@vtech.fr>
2020-03-27 19:10:24 +0100

    zsh prompt: lets try less visual clutter (no more bold, no more big dot)

--- .oh-my-zsh/themes/leimi.zsh
+++ .oh-my-zsh/themes/leimi.zsh
@@ -7,8 +7,8 @@ autoload -Uz vcs_info
 darkgrey=$FG[244]
 lightgrey=$FG[248]
 
-zstyle ':vcs_info:*' stagedstr '%F{green}●'
-zstyle ':vcs_info:*' unstagedstr '%F{yellow}●'
+zstyle ':vcs_info:*' stagedstr '%F{green}%B^'
+zstyle ':vcs_info:*' unstagedstr '%F{yellow}%B^'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b:%F{2}%r'
 zstyle ':vcs_info:*' enable git svn
@@ -16,14 +16,14 @@ theme_precmd () {
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
         zstyle ':vcs_info:*' formats '%b%c%u%F{green} '
     } else {
-        zstyle ':vcs_info:*' formats '%b%c%u%F{red}●%F{green} '
+        zstyle ':vcs_info:*' formats '%b%c%u%F{red}%B^%F{green} '
     }
 
     vcs_info
 }
 
 setopt prompt_subst
-PROMPT='%F{green}${vcs_info_msg_0_}%B%F{yellow}%1~%{$reset_color%} '
+PROMPT='%F{green}${vcs_info_msg_0_}%F{yellow}%1~%{$reset_color%} '
 RPROMPT='%{$darkgrey%}%*%{$reset_color%}'
 
 autoload -U add-zsh-hook

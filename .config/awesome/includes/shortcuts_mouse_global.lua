dc65a7432a41a542a9f05fc8ce82be6acdebaf46
Emmanuel Pelletier <manu@habite.la>
2023-03-08 14:53:17 +0100

    who cares

--- .config/awesome/includes/shortcuts_mouse_global.lua
+++ .config/awesome/includes/shortcuts_mouse_global.lua
@@ -1,6 +1,4 @@
 -- default mouse bindings that work everywhere
 root.buttons(gears.table.join(
-  awful.button({ }, 3, function() main_menu:toggle() end),
-  awful.button({ }, 4, awful.tag.viewnext),
-  awful.button({ }, 5, awful.tag.viewprev)
+  awful.button({ }, 3, function() main_menu:toggle() end)
 ))

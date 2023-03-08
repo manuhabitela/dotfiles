bc414aa9d3642ccfde83857ebc03f1f2df4d3b1d
Emmanuel Pelletier <manu@habite.la>
2023-03-08 14:50:05 +0100

    change autostart stuff

--- .config/awesome/includes/autostart.lua
+++ .config/awesome/includes/autostart.lua
@@ -1,8 +1,9 @@
 -- autostarting apps - awesome_boot loads every .desktop file in standard autostart folder
 -- awful.spawn.with_shell("awesome_boot")
 -- I just *can't* manage to start xmodmap at boot correctly, so, putting that here...
-helpers.launcher.run_once("xmodmap ~/.xmodmaprc")
--- helpers.launcher.run_once("picom -b")
-helpers.launcher.run_once("~/bin/xtrempro-dirty-idle-fix")
+helpers.launcher.run_once("/home/manu/bin/dynamic-xmodmap")
+helpers.launcher.run_once("/home/manu/bin/xtrempro-dirty-idle-fix")
+helpers.launcher.run_once("redshift-gtk")
 helpers.launcher.run_once("pasystray")
+helpers.launcher.run_once("cbatticon")
 helpers.launcher.run_once(terminal)
